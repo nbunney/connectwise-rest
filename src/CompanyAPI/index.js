f5b1c218bb72d72adba0426844f093e75684eb48
Nathan Bunney <nathan@Inventaweb.net>
2018-02-01 16:44:16 -0800

    Added Company.Teams

--- src/CompanyAPI/index.js
+++ src/CompanyAPI/index.js
@@ -19,12 +19,14 @@
 function CompanyAPI(options) {
   var _Companies = require('./Companies'),
     _Contacts = require('./Contacts'),
+    _Teams = require('./CompanyTeams'),
     _Configurations = require('./Configurations');
   //_ContactRelationships = require('./ContactRelationships')
 
   return {
     Companies: new _Companies(options),
     Contacts: new _Contacts(options),
+    Teams: new _Teams(options),
     Configurations: new _Configurations(options)
     //ContactRelationships: new _ContactRelationships(options)
   }
